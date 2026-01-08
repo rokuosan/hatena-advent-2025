908086e22fc1a2d46b09464d3bbfdba96a575bfc
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2026-01-08 21:05:42 +0900

    change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ImageLoader } from "./image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello, Next.js!</h1>
+      <h1 className="text-3xl font-bold">node-vibrant example</h1>
       <ImageLoader />
     </div>
   );
